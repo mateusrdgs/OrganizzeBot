c6b3909900e5d227208ce7a00e84ef16111b837d
Mateus Rodrigues <mateusrdgs@protonmail.com>
2025-08-19 14:25:06 -0300

    feat(labelling): prepending HANDLE_MANUALLY into expenses with multiple installments

--- Labelling/main.py
+++ Labelling/main.py
@@ -17,6 +17,8 @@ labelled_file = os.getenv("LABELLED_CSV", "") + file_extension
 df_unlabeled_path = os.path.join("..", "./Private", unlabelled_file)
 df_labeled_path = os.path.join("..", "./Private", labelled_file)
 
+installments_sentence_regex = r" - Parcela \d\/\d+"
+
 # Load CSVs
 df_unlabeled = pd.read_csv(df_unlabeled_path)
 df_labeled = pd.read_csv(df_labeled_path)
@@ -47,7 +49,7 @@ def clean_transaction_name(name: str) -> str:
     name = unicodedata.normalize("NFKD", name).encode("ascii", "ignore").decode("utf-8", "ignore")
 
     # Remove "Installments" from sentence
-    name = re.sub(r" - Parcela \d\/\d+", "", name)
+    name = re.sub(installments_sentence_regex, "", name)
     name = re.sub(r" \d\/\d+", "", name)
 
     return name
@@ -71,16 +73,6 @@ def predict_expense_category(name, model, vectorizer, lookup_dict, threshold=0.7
 # Filter out installments other than the first one
 df_unlabeled = df_unlabeled[(~df_unlabeled["name"].str.contains(r"- Parcela \b(?!1\b)\d+\b\/\d"))]
 
-# Update amounts for the remaining expenses with installments
-def multiply_amount(name, amount):
-    if (re.search(r" - Parcela \d\/\d+", name)):
-        installment = int(re.sub(r".* - Parcela \d\/", "", name))
-        return installment * amount
-
-    return amount
-
-df_unlabeled['amount'] = df_unlabeled.apply(lambda row: multiply_amount(row["name"], row.amount), axis=1)
-
 # Clean up expenses names to see if their name match with an existing one in the dictionary
 unlabeled_expenses_names = df_unlabeled["name"].astype(str).apply(clean_transaction_name)
 
@@ -93,6 +85,25 @@ for name in unlabeled_expenses_names:
 
 df_unlabeled["category"] = final_preds
 df_unlabeled["tags"] = os.getenv("TAGS", "")
+
+# Update amounts for the remaining expenses with installments
+def multiply_amount(name, amount):
+    if (re.search(installments_sentence_regex, name)):
+        installment = int(re.sub(r".* - Parcela \d\/", "", name))
+        return installment * amount
+
+    return amount
+
+df_unlabeled['amount'] = df_unlabeled.apply(lambda row: multiply_amount(row["name"], row.amount), axis=1)
+
+# Update name for expenses with installments so I easily spot and handle them later
+def update_name_expenses_with_installments(name):
+    if (re.search(installments_sentence_regex, name)):
+        return "HANDLE MANUALLY - " + name
+
+    return name
+
+df_unlabeled['name'] = df_unlabeled.apply(lambda row: update_name_expenses_with_installments(row["name"]), axis=1)
 
 # Save DataFrame as CSV
 new_file = os.getenv("UNLABELLED_CSV", "") + " - labelled" + file_extension
