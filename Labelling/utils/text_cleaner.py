a4fc23c5cf64deb4014bc4f19d3de3749c7d2d53
Mateus Rodrigues <mateusrdgs@protonmail.com>
2025-10-18 18:17:39 -0300

    refactor(labelling): updating variable name

--- Labelling/utils/text_cleaner.py
+++ Labelling/utils/text_cleaner.py
@@ -28,20 +28,20 @@ def update_name_expenses_with_installments(name):
 
     return name
 
-def postprocess(UNLABELLED_DF):
+def postprocess(unlabelled_df):
   EXCLUSION_LIST = set(os.getenv("EXCLUSION_LIST", "").split(";"))
 
-  UNLABELLED_DF['amount'] = UNLABELLED_DF.apply(lambda row: multiply_amount(row["name"], row.amount), axis=1)
+  unlabelled_df['amount'] = unlabelled_df.apply(lambda row: multiply_amount(row["name"], row.amount), axis=1)
 
-  UNLABELLED_DF['name'] = UNLABELLED_DF.apply(lambda row: update_name_expenses_with_installments(row["name"]), axis=1)
+  unlabelled_df['name'] = unlabelled_df.apply(lambda row: update_name_expenses_with_installments(row["name"]), axis=1)
 
   # Filter out installments other than the first one
-  UNLABELLED_DF = UNLABELLED_DF[(~UNLABELLED_DF["name"].str.contains(r"- Parcela \b(?!1\b)\d+\b\/\d"))]
+  unlabelled_df = unlabelled_df[(~unlabelled_df["name"].str.contains(r"- Parcela \b(?!1\b)\d+\b\/\d"))]
 
   # Filter out expenses in exclusion list
-  UNLABELLED_DF = UNLABELLED_DF[UNLABELLED_DF["name"].apply(lambda x: x not in EXCLUSION_LIST)]
+  unlabelled_df = unlabelled_df[unlabelled_df["name"].apply(lambda x: x not in EXCLUSION_LIST)]
 
   # Apply tags
-  UNLABELLED_DF["tags"] = os.getenv("TAGS", "")
+  unlabelled_df["tags"] = os.getenv("TAGS", "")
 
-  return UNLABELLED_DF
+  return unlabelled_df
