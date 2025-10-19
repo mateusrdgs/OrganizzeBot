6618e3d88188b50b378c1c4ed995cd471492356b
Mateus Rodrigues <mateusrdgs@protonmail.com>
2025-10-19 11:34:08 -0300

    refactor(labelling): updating variable name

--- Labelling/__main__.py
+++ Labelling/__main__.py
@@ -4,19 +4,19 @@ from utils.text_cleaner import postprocess
 from utils.data_loader import load_datasets
 
 def main():
-    UNLABELLED_DF, LABELLED_DF = load_datasets()
+    unlabelled_df, labelled_df = load_datasets()
 
     # Predict expenses categories
-    UNLABELLED_DF = predict(UNLABELLED_DF, LABELLED_DF)
+    unlabelled_df = predict(unlabelled_df, labelled_df)
 
     # Postprocess DataFrame
-    UNLABELLED_DF = postprocess(UNLABELLED_DF)
+    unlabelled_df = postprocess(unlabelled_df)
 
     # Save DataFrame as CSV
     new_file = os.getenv("UNLABELLED_CSV", "") + " - labelled" + ".csv"
     new_file_path = os.path.join("./Private", new_file)
 
-    UNLABELLED_DF.to_csv(new_file_path, index=False, float_format="%.2f", header=False)
+    unlabelled_df.to_csv(new_file_path, index=False, float_format="%.2f", header=False)
 
 if __name__ == '__main__':
     main()
