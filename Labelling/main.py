bfc4096dd03a31d0c6d0a60d7c5b5806469a3b49
Mateus Rodrigues <mateusrdgs@protonmail.com>
2025-10-21 20:37:53 -0300

    fix(labelling): correctly loading env variables on startup

--- Labelling/main.py
+++ Labelling/main.py
@@ -1,17 +1,25 @@
+from dotenv import load_dotenv
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from Labelling.api.model import controller as model_controller
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(_app: FastAPI):
+  load_dotenv()
+  yield
+
+app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
   CORSMiddleware,
   allow_origins=[
-    "http://localhost:8000",
+    "http://localhost",
   ],
   allow_methods=[
     "POST"
   ]
 )
+
 app.include_router(model_controller.router)
