aaec792ec04cc09e7f2002ad643fae16adafe045
Mateus Rodrigues <mateusrdgs@protonmail.com>
2025-11-12 12:31:13 -0300

    refactor(server): validating uploaded file

--- server/src/labelling/labelling.controller.ts
+++ server/src/labelling/labelling.controller.ts
@@ -1,10 +1,14 @@
 import {
+  HttpCode,
   BadRequestException,
   Controller,
   Post,
   UploadedFile,
   UseInterceptors,
   ClassSerializerInterceptor,
+  ParseFilePipe,
+  FileTypeValidator,
+  MaxFileSizeValidator,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 
@@ -14,8 +18,22 @@ import { LabellingService } from './labelling.service';
 export class LabellingController {
   constructor(private labellingService: LabellingService) {}
   @Post('')
+  @HttpCode(200)
   @UseInterceptors(ClassSerializerInterceptor, FileInterceptor('file'))
-  async parseExpenses(@UploadedFile() file: Express.Multer.File) {
+  async parseExpenses(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 100000 }),
+          new FileTypeValidator({
+            fileType: 'text/csv',
+            skipMagicNumbersValidation: true,
+          }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
     if (file) {
       const [error, expenses] = await this.labellingService.parseExpenses(file);
 
