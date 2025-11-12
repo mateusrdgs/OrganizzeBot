d2f0f84fbdb7aaae01d5eaac834c31e970277d54
Mateus Rodrigues <mateusrdgs@protonmail.com>
2025-11-12 12:31:25 -0300

    feat(server): adding helmet

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,10 @@
+import helmet from 'helmet';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   await app.listen(process.env.PORT ?? 3000);
 }
 
